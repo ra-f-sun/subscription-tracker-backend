9e2ac9f7d423932bbe376d2b36002cef4579359c
Rafsun Jani <1999rafsun@gmail.com>
2025-03-14 02:32:41 +0600

    server initialized

---
+++ app.js
@@ -0,0 +1,28 @@
+import express, { urlencoded } from "express";
+import { PORT } from "./config/env.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(errorMiddleware);
+
+// Routes
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.listen(PORT, async () => {
+  console.log(`running the server on ${PORT}`);
+  await connectToDatabase();
+});
+
+export default app;
---
+++ models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "User name is required"],
+      trim: true,
+      minLength: 2,
+      maxLength: 30,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: 6,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      lowercase: true,
+      unique: true,
+      trim: true,
+      match: [/\S+@\S+\.\S+/, "Please fill a valid email address"],
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
